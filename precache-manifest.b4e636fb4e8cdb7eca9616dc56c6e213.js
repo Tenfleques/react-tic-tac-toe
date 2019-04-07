e49dabf8a4ccfa628d37ce6005cd807a9e5bb940
Tendai Chikake <tendaichikae@gmail.com>
2019-04-07 03:27:52 +0300

    Updates

---
+++ precache-manifest.b4e636fb4e8cdb7eca9616dc56c6e213.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "6c5ab99df12010577769",
+    "url": "/tic-tac-toe/static/css/main.5d0d99d0.chunk.css"
+  },
+  {
+    "revision": "6c5ab99df12010577769",
+    "url": "/tic-tac-toe/static/js/main.83e7a461.chunk.js"
+  },
+  {
+    "revision": "9728fa41a930f12fdc87",
+    "url": "/tic-tac-toe/static/js/runtime~main.f1e29306.js"
+  },
+  {
+    "revision": "70666f50ac4a477bdc23",
+    "url": "/tic-tac-toe/static/js/2.16da3acb.chunk.js"
+  },
+  {
+    "revision": "66b72f435236c5caf1f2050c78686497",
+    "url": "/tic-tac-toe/index.html"
+  }
+];
